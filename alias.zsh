560a1ccee003f1fd4a10c8b8c0e059d5b6f84415
Matt Bisson <mbisson@vmware.com>
2016-05-11 18:31:25 -0400

    Provide default parameters for the "less" command that make Git easier to use.

--- alias.zsh
+++ alias.zsh
@@ -34,6 +34,9 @@ fi
 if [ -x '/usr/local/bin/less' ] || [ -x '/usr/bin/less' ] ; then
     alias  more=less                    # Less IS more...
     export PAGER=less
+    # -X tends to needlessly clear the screen, and -F lets a single screenful
+    # -print and quit...
+    export LESS="${LESS} -F -X"
 fi
 
 # TTY Setting aliases
